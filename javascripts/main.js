93199c55caa1496ee0a607f60f52ecb496767e2d
hzwuyuedong <hzwuyuedong@corp.netease.com>
2014-08-19 22:00:45 +0800

    page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+$('.countdown_dashboard')
+  .eq(0).countdown().end()
+  .eq(1).countdown({
+    year: 0,
+    month: 0,
+    day: 1,
+    hour: 0,
+    min: 10,
+    sec: 1,
+  }).end()
+  .eq(2).countdown({
+    diff: 20,
+    onEnd: function () {
+      alert('End')
+    }
+  }).end();
+
